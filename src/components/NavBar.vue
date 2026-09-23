eb8dc3d5493e6e1c8a35712c8d9085529b4a7627
ajs <140010746+aboodJS@users.noreply.github.com>
2026-09-23 22:52:51 +0300

    added some logic to the navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,20 @@
+<script setup>
+const isLoggedIn = localStorage.getItem("jwt") ? true : false;
+</script>
+
 <template>
   <nav
     class="flex h-[10vh] w-screen items-center justify-between fixed top-0 text-white bg-black text-2xl"
   >
     <h3 class="font-extrabold px-3">notes</h3>
-    <div class="flex px-3 gap-3">
+    <div v-if="isLoggedIn === false" class="flex px-3 gap-3">
       <RouterLink to="/login"><p class="hover:underline">login</p></RouterLink>
       <RouterLink to="/signup"
         ><p class="hover:underline">signup</p></RouterLink
       >
+    </div>
+    <div v-else>
+      <button>Signout</button>
     </div>
   </nav>
 </template>
